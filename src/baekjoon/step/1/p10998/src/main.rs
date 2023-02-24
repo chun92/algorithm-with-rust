75a7dfb6680a216fb4c1c16c41630b63e8a212c6
Chunuiyu <chunuiyu@gmail.com>
2023-02-25 00:57:39 +0900

    solve: 10998

---
+++ src/baekjoon/step/1/p10998/src/main.rs
@@ -0,0 +1,17 @@
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let numbers:Vec<i32> = input
+        .trim()
+        .split_whitespace()
+        .map(|x| x.parse().unwrap())
+        .collect();
+    
+    let mut iter = numbers.iter();
+    let first = iter.next().unwrap();
+    let second = iter.next().unwrap();
+    let result = first * second;
+    println!("{}", result);
+}
