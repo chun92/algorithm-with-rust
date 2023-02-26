9fa2b8cef6b7e14a19d8f1f8c216eff6dd8745df
Chunuiyu <chunuiyu@gmail.com>
2023-02-26 11:39:43 +0900

    solve: 2908

--- src/baekjoon/step/5/p2908/src/main.rs
+++ src/baekjoon/step/5/p2908/src/main.rs
@@ -1,3 +1,29 @@
+use std::io;
+
+fn read_line_as_swap_and_print_numbers() {
+    let mut s = String::new();
+    io::stdin().read_line(&mut s).unwrap();
+
+    let vec:Vec<i32> = s
+        .trim()
+        .split_whitespace()
+        .map(|x| reverse_str(x)
+            .parse::<i32>()
+            .unwrap())
+        .collect();
+    let result = std::cmp::max(vec[0], vec[1]);
+    println!("{}", result);
+}
+
+fn reverse_str(target: &str) -> String {
+    let string = String::from(target);
+    let result = string
+        .chars()
+        .rev()
+        .collect();
+    result
+}
+
 fn main() {
-    println!("Hello, world!");
+    read_line_as_swap_and_print_numbers();
 }
