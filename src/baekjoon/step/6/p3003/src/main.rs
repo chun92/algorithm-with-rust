7ad98b8338f79eea774e74b525c457d097953bf0
Chunuiyu <chunuiyu@gmail.com>
2023-02-26 17:42:12 +0900

    solve: 3003

--- src/baekjoon/step/6/p3003/src/main.rs
+++ src/baekjoon/step/6/p3003/src/main.rs
@@ -1,3 +1,35 @@
+use std::io;
+
+fn read_line_as_numbers() -> Vec<i32> {
+    let mut s = String::new();
+    io::stdin().read_line(&mut s).unwrap();
+    
+    let values:Vec<i32> = s
+        .trim()
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+    values
+}
+
+fn print_vector<T: std::fmt::Display>(vec:Vec<T>) {
+    for (i, v) in vec.iter().enumerate() {
+        if i < vec.len() - 1 {
+            print!("{} ", v);
+        } else {
+            println!("{}", v);
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let basis = vec![1, 1, 2, 2, 2, 8];
+    let numbers = read_line_as_numbers();
+    let results:Vec<i32> = basis
+        .iter()
+        .zip(numbers.iter())
+        .map(|(x, y)| {
+            x - y
+        }).collect();
+    print_vector(results);
 }
