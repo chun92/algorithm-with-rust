abc771855b8d626120edd0b68f6a27eda4d97d7c
Chunuiyu <chunuiyu@gmail.com>
2023-02-27 14:15:56 +0900

    chore: improve tools

--- tool/al_done.bash
+++ tool/al_done.bash
@@ -12,7 +12,7 @@ function check_current_directory_and_git_push_all() {
     if [ -d "$subdir" ]; then # 서브 디렉토리가 존재하면
       git status $subdir | grep "Untracked files:\|modified:" # 변경된 파일이 있는지 확인
       if [ $? -eq 0 ]; then # 변경된 파일이 있으면
-        git_push $subdir
+        git_push $(basename $subdir)
       fi
     fi
   done
