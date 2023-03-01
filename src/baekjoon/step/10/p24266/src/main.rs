851bf6b392bc58996627d04a456e3b06272a4051
Chunuiyu <chunuiyu@gmail.com>
2023-03-01 22:28:28 +0900

    solve: 24266

---
+++ src/baekjoon/step/10/p24266/src/main.rs
@@ -0,0 +1,11 @@
+fn read_line_as_number() -> u64 {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    input.trim().parse::<u64>().unwrap()
+}
+
+fn main() {
+    let n = read_line_as_number();
+    println!("{}", n.pow(3));
+    println!("3");
+}
