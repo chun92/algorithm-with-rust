5b036c64a59967fcb6cbbb3e70a98453bae79e3b
Chunuiyu <chunuiyu@gmail.com>
2023-03-03 01:05:21 +0900

    solve: 2750

---
+++ src/baekjoon/step/11/p2750/src/main.rs
@@ -0,0 +1,18 @@
+fn read_line_as_number() -> i32 {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    input.trim().parse::<i32>().unwrap()
+}
+
+fn main() {
+    let n = read_line_as_number();
+    let mut vec = Vec::new();
+    for _ in 0..n {
+        vec.push(read_line_as_number());
+    }
+
+    vec.sort();
+    vec
+        .iter()
+        .for_each(|x| println!("{}", *x));
+}
