b333b2dc4fc8f26d937e388a5cdef25720fd03a9
Chunuiyu <chunuiyu@gmail.com>
2023-03-05 22:10:09 +0900

    solve: 15652

---
+++ src/baekjoon/p15652/src/main.rs
@@ -0,0 +1,35 @@
+use std::fmt::Write;
+
+fn read_line_as_numbers() -> Vec<i32> {
+    let mut line = String::new();
+    std::io::stdin().read_line(&mut line).unwrap();
+    line.split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect()
+}
+
+fn print_num(out: &mut String, numbers: &mut Vec<usize>, index: usize, limit: usize, remaining: i32) {
+    if remaining == 0 {
+        let result = numbers
+        .iter()
+        .map(|x| x.to_string())
+        .collect::<Vec<String>>()
+        .join(" ");
+        writeln!(out, "{}", result).unwrap();
+    } else {
+        for i in index..= limit {
+            numbers.push(i);
+            print_num(out, numbers, i, limit, remaining - 1);
+            numbers.pop();
+        }
+    }
+}
+
+fn main() {
+    let mut out = String::new();
+    let args = read_line_as_numbers();
+    let (n, m) = (args[0], args[1]);
+    let mut numbers = Vec::new();
+    print_num(&mut out, &mut numbers, 1, n as usize, m);
+    print!("{}", out);
+}
