7ba36c145e945202c5bf1aad60d9edfaa906c916
Chunuiyu <chunuiyu@gmail.com>
2023-03-06 18:04:18 +0900

    solve: 15964

---
+++ src/baekjoon/p15964/src/main.rs
@@ -0,0 +1,15 @@
+fn read_line_as_numbers() -> Vec<i64> {
+    let mut line = String::new();
+    std::io::stdin().read_line(&mut line).unwrap();
+    line.split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect()
+}
+
+fn main() {
+    let numbers = read_line_as_numbers();
+    let (n, m) = (numbers[0], numbers[1]);
+
+    let result = (n + m) * (n - m);
+    println!("{}", result);
+}
