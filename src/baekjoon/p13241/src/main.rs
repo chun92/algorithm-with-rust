97ecdda5731193f7f621387c06d2ff14d06b0bc6
Chunuiyu <chunuiyu@gmail.com>
2023-03-13 00:43:58 +0900

    solve: 13241

---
+++ src/baekjoon/p13241/src/main.rs
@@ -0,0 +1,22 @@
+fn read_line_as_numbers() -> Vec<u64> {
+    let mut line = String::new();
+    std::io::stdin().read_line(&mut line).unwrap();
+    line.split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect()
+}
+
+fn gcd(a: u64, b: u64) -> u64 {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
+
+fn main() {
+    let numbers = read_line_as_numbers();
+    let (n, m) = (numbers[0], numbers[1]);
+    let res = n / gcd(n, m) * m;
+    println!("{}", res);
+}
