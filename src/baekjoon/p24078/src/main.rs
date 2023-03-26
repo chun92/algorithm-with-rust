107618950ed6a6d0ed0fe2514e12e648ec652f48
Chunuiyu <chunuiyu@gmail.com>
2023-03-27 01:33:44 +0900

    solve: 24078

---
+++ src/baekjoon/p24078/src/main.rs
@@ -0,0 +1,9 @@
+fn read_line_as_number() -> i32 {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    input.trim().parse::<i32>().unwrap()
+}
+
+fn main() {
+    println!("{}", read_line_as_number() % 21);
+}
