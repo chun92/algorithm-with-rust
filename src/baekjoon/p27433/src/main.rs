39bb7df2cb9af75dace90a7f157e420e87ee6bc7
Chunuiyu <chunuiyu@gmail.com>
2023-04-05 00:42:03 +0900

    solve: rc

---
+++ src/baekjoon/p27433/src/main.rs
@@ -0,0 +1,18 @@
+fn read_line_as_number() -> u64 {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    input.trim().parse::<u64>().unwrap()
+}
+
+fn factorial(n: u64) -> u64 {
+    if n == 0 {
+        1
+    } else {
+        n * factorial(n - 1)
+    }
+}
+
+fn main() {
+    let n = read_line_as_number();
+    println!("{}", factorial(n));
+}
