c7a903e63f48f6ed8af355812ecb124e6f1bf15f
Chunuiyu <chunuiyu@gmail.com>
2023-04-13 02:14:59 +0900

    solve: 10991

---
+++ src/baekjoon/p10991/src/main.rs
@@ -0,0 +1,28 @@
+fn read_line_as_number() -> i32 {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    input.trim().parse::<i32>().unwrap()
+}
+
+fn main() {
+    let n = read_line_as_number();
+    
+    for i in 0..n {
+        for _ in 0..(n - i - 1) {
+            print!(" ");
+        }
+        print!("*");
+        if i == 0 {
+            println!();
+            continue;
+        }
+        for j in 0..(2 * i - 1) {
+            if j % 2 == 0 {
+                print!(" ");
+            } else {
+                print!("*");
+            }
+        }
+        println!("*");
+    }
+}
