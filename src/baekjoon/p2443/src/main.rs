3b73669df0502dbe5cc883711f099cdd8b58cd22
Chunuiyu <chunuiyu@gmail.com>
2023-04-13 01:40:32 +0900

    solve: 2443

---
+++ src/baekjoon/p2443/src/main.rs
@@ -0,0 +1,18 @@
+fn read_line_as_number() -> i32 {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    input.trim().parse::<i32>().unwrap()
+}
+
+fn main() {
+    let n = read_line_as_number();
+    for i in 0..n {
+        for _ in 0..i {
+            print!(" ");
+        }
+        for _ in 0..(2 * (n - i) - 1) {
+            print!("*");
+        }
+        println!();
+    }
+}
