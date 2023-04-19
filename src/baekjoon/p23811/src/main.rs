7deb4935decd98655363de5cb69abcea89d23763
Chunuiyu <chunuiyu@gmail.com>
2023-04-20 01:21:43 +0900

    solve: 23811

---
+++ src/baekjoon/p23811/src/main.rs
@@ -0,0 +1,25 @@
+use std::io::{stdin, stdout, Write, BufWriter};
+
+fn read_line_as_number() -> usize {
+    let mut input = String::new();
+    let _ = stdout().flush();
+    stdin().read_line(&mut input).unwrap();
+    input.trim().parse().unwrap()
+}
+
+fn main() {
+    let n = read_line_as_number();
+    let mut writer = BufWriter::new(stdout());
+
+    for i in 0..5 * n {
+        for j in 0..5 * n {
+            let i = i / n % 5;
+            let j = j / n % 5;
+
+            if i == 0 || i == 2 || i == 4 || j == 0 {
+                write!(writer, "@").unwrap();
+            }
+        }
+        writeln!(writer).unwrap();
+    }
+}
