12ce1b9181f282599db3cca31573409d7f9bb7bc
Chunuiyu <chunuiyu@gmail.com>
2023-04-20 01:21:23 +0900

    solve: 23802

---
+++ src/baekjoon/p23802/src/main.rs
@@ -0,0 +1,22 @@
+use std::io::{stdin, stdout, Write, BufWriter};
+
+fn read_line_as_number() -> usize {
+    let mut input = String::new();
+    let _ = stdout().flush();
+    stdin().read_line(&mut input).unwrap();
+    input.trim().parse().unwrap()
+}
+
+fn main() {
+    let n = read_line_as_number();
+    let mut output = BufWriter::new(stdout());
+
+    for i in 0..5 * n {
+        for j in 0..5 * n {
+            if i / n % 5 == 0 || j / n % 5 == 0 {
+                write!(output, "@").unwrap();
+            }
+        }
+        writeln!(output).unwrap();
+    }
+}
