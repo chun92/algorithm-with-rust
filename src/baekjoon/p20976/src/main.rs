2e797210d5083b2f44b54219c7bde2772c694eac
Chunuiyu <chunuiyu@gmail.com>
2023-04-20 01:36:59 +0900

    solve: 20976

---
+++ src/baekjoon/p20976/src/main.rs
@@ -0,0 +1,13 @@
+fn read_line_as_numbers() -> Vec<i32> {
+    let mut line = String::new();
+    std::io::stdin().read_line(&mut line).unwrap();
+    line.split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect()
+}
+
+fn main() {
+    let mut numbers = read_line_as_numbers();
+    numbers.sort();
+    println!("{}", numbers[1]);
+}
