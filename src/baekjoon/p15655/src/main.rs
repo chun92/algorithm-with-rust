edbb2bb7f64afa441405cbcfebeb97258aca1242
Chunuiyu <chunuiyu@gmail.com>
2023-04-22 02:13:42 +0900

    solve: 15655

---
+++ src/baekjoon/p15655/src/main.rs
@@ -0,0 +1,44 @@
+use std::io::{stdin, stdout, Write, BufWriter};
+
+fn read_line_as_numbers() -> Vec<usize> {
+    let mut s = String::new();
+    stdin().read_line(&mut s).unwrap();
+    s.trim().split_whitespace().map(|x| x.parse().unwrap()).collect()
+}
+
+fn main() {
+    let (n, m) = {
+        let args = read_line_as_numbers();
+        (args[0], args[1])
+    };
+
+    let mut numbers = read_line_as_numbers();
+    numbers.sort();
+
+    let mut output = BufWriter::new(stdout());
+    let mut current = m - 1;
+    let mut indexes = (0..m).map(|i| i).collect::<Vec<_>>();
+    'outer: loop {
+        let str: String = indexes
+            .iter()
+            .map(|i| numbers[*i].to_string())
+            .collect::<Vec<_>>()
+            .join(" ");
+        
+        writeln!(output, "{}", str).unwrap();
+
+        while indexes[current] == n - m + current {
+            if current != 0 {
+                current -= 1;
+            } else {
+                break 'outer;
+            }
+        }
+
+        indexes[current] += 1;
+        for i in current + 1..m {
+            indexes[i] = indexes[i - 1] + 1;
+        }
+        current = m - 1;
+    }
+}
