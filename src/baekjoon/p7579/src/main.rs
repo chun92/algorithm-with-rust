c1db13b55e2fd07f36e6269696cb26d8916b3787
Chunuiyu <chunuiyu@gmail.com>
2023-04-26 21:29:38 +0900

    fix: fix 7579

--- src/baekjoon/p7579/src/main.rs
+++ src/baekjoon/p7579/src/main.rs
@@ -1,4 +1,4 @@
-fn read_line_as_numbers() -> Vec<i32> {
+fn read_line_as_numbers() -> Vec<u32> {
     let mut line = String::new();
     std::io::stdin().read_line(&mut line).unwrap();
     line.split_whitespace()
@@ -6,8 +6,8 @@ fn read_line_as_numbers() -> Vec<i32> {
         .collect()
 }
 
-fn solve(memories: &Vec<i32>, costs: &Vec<i32>, n: i32, m: i32) -> i32 {
-    let cost_sum = costs.iter().sum::<i32>();
+fn solve(memories: &Vec<u32>, costs: &Vec<u32>, n: u32, m: u32) -> u32 {
+    let cost_sum = costs.iter().sum::<u32>();
     let mut memo = vec![vec![0; cost_sum as usize]; n as usize];
 
     let mut start = 0;
@@ -29,10 +29,10 @@ fn solve(memories: &Vec<i32>, costs: &Vec<i32>, n: i32, m: i32) -> i32 {
     mid
 }
 
-fn dp(memo: &mut Vec<Vec<i32>>, memories: &Vec<i32>, costs: &Vec<i32>, i: usize, c: i32) -> i32 {
+fn dp(memo: &mut Vec<Vec<u32>>, memories: &Vec<u32>, costs: &Vec<u32>, i: usize, c: u32) -> u32 {
     if i == 0 {
         if c < costs[0] {
-            return -1;
+            return 0;
         } else {
             return memories[0];
         }
@@ -46,7 +46,7 @@ fn dp(memo: &mut Vec<Vec<i32>>, memories: &Vec<i32>, costs: &Vec<i32>, i: usize,
     let res2 = if c >= costs[i] {
         dp(memo, memories, costs, i - 1, c - costs[i]) + memories[i]
     } else {
-        -1
+        0
     };
 
     let res = std::cmp::max(res1, res2);
@@ -62,5 +62,5 @@ fn main() {
 
     let memories = read_line_as_numbers();
     let costs = read_line_as_numbers();
-    println!("{}", solve(&memories, &costs, n as i32, m));
+    println!("{}", solve(&memories, &costs, n as u32, m));
 }
