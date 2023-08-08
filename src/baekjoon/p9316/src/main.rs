1301cb02512b72f0ed2a95e9692603a95080a23f
Chunuiyu <chunuiyu@gmail.com>
2023-08-08 23:36:40 +0900

    solve: 9316

---
+++ src/baekjoon/p9316/src/main.rs
@@ -0,0 +1,13 @@
+fn read_line_as_number() -> usize {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    input.trim().parse().unwrap()
+}
+
+fn main() {
+    let n = read_line_as_number();
+
+    for i in 1..=n {
+        println!("Hello World, Judge {}!", i);
+    }
+}
